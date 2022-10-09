b4d28f915593f141dc13703479c2e692baa4aedb
cuongha12 <cuongbeeg1234@gmail.com>
2022-10-10 01:53:12 +0700

    changes

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.get('/', (req, res) => {
     var conectFile = path.join(__dirname, 'home.html')
     res.sendFile(conectFile)
 })
-
+app.get('/home', (req, res) => {
+    res.json("home")
+})
 app.listen(process.env.PORT, () => {
 
 })
