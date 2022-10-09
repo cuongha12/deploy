3b684be35a69338167f49d55b6d031cef89c60db
cuongha12 <cuongbeeg1234@gmail.com>
2022-10-10 01:32:15 +0700

    change

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+app.use('/public',express.static(path.join(__dirname, '/public')))
+app.get('/', (req, res) => {
+    var conectFile = path.join(__dirname, 'home.html')
+    res.sendFile(conectFile)
+})
+
+app.listen(8000, () => {
+
+})
